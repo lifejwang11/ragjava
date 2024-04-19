ccffc9ec136a7988cdb7161f467a55466b9c5b22
life <13122192336@163.com>
2024-04-19 16:39:14 +0800

    提交视图

--- code/java/rag/src/main/java/com/life/rag/controller/QuestionController.java
+++ code/java/rag/src/main/java/com/life/rag/controller/QuestionController.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("question")
 public class QuestionController {
 
